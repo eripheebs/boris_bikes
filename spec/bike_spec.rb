e31c6e14521cd954ff687bb8d6b318b33ababaf4
eripheebs <erikapheby@gmail.com>
2016-03-01 11:37:37 +0000

    Added spec tests for releasing the bike and checking if the bike is working

---
+++ spec/bike_spec.rb
@@ -0,0 +1,8 @@
+require '../lib/bike.rb'
+
+describe 'working?' do
+
+	it 'expect working bike to return true' do
+	end
+	
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,14 @@
 require '../lib/docking_station.rb'
+require '../lib/bike.rb'
 
 describe 'release_bike' do
 
 	it 'expect docking station to release bike' do
+		expect(release_bike).to eq (Bike.new)
+	end
+
+	it 'expects bike released to be working' do
+		expect(Bike.new.working?).to eq (true)
 	end
 	
 end
